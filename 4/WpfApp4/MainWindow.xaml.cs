1c35a74661a1229dea3d9cbf62252deb5ab8417a
KesArtem <77680598+KesArtem@users.noreply.github.com>
2021-04-13 20:38:25 +0300

    Add files via upload

---
+++ 4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void T2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void Slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (t1 != null)
+                t2.FontSize = ((Slider)sender).Value;
+        }
+    }
+}
